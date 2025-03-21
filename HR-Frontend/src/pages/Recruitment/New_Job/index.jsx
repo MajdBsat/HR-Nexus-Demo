627f17aa654453c65afb3b52d78996bfe1e56ff5
Mohammad Zeineddine <mohammad.zeineddine50@gmail.com>
2025-03-22 00:37:53 +0200

    added new job design

--- HR-Frontend/src/pages/Recruitment/New_Job/index.jsx
+++ HR-Frontend/src/pages/Recruitment/New_Job/index.jsx
@@ -1,9 +1,12 @@
+import { useNavigate } from "react-router-dom";
 import "./index.css"
 
 const Add_new_job = () =>{
+    const navigate = useNavigate();
     const cancel = (e) =>{
         e.preventDefault()
         console.log('Cancel')
+        navigate("/hr/recruitment/jobs/")
     }
 
     const addJob = (e) =>{
@@ -11,23 +14,23 @@ const Add_new_job = () =>{
         console.log('Add')
     }
     return(
-        <div className="flex column center width100vw height100vh">
+        <div className="back-div flex column center width100vw height100vh">
             <form className="flex column center">
                 <h2 className="form_title">Add New Job</h2>
-                <div className="label_input">
+                <div className="label_input flex column width50">
                     <label htmlFor="title">Job Title</label>
-                    <input type="text" placeholder="Software Engineer"/>
+                    <input id="title" type="text" placeholder="Software Engineer"/>
                 </div>
-                <div className="label_input">
+                <div className="label_input flex column width50">
                     <label htmlFor="description">Job Description</label>
-                    <input type="text" placeholder="We are seeking a skilled and detail-oriented Programmer to join our development team."/>
+                    <textarea rows={10} id="description" type="text" placeholder="We are seeking a skilled and detail-oriented Programmer to join our development team."/>
                 </div>
-                <div className="label_input">
+                <div className="label_input flex column width50">
                     <label htmlFor="requirements">Job Requirements</label>
-                    <input type="text" placeholder="HTML, CSS, JS, PHP, Laravel"/>
+                    <textarea rows={10} id="requirements" type="text" placeholder="HTML, CSS, JS, PHP, Laravel"/>
                 </div>
 
-                <div className="buttons flex row center">
+                <div className="buttons flex row center width50">
                     <button className="cancel-btn" onClick={(e)=>cancel(e)}>Cancel</button>
                     <button className="add-btn" onClick={(e)=>addJob(e)}>Add</button>
                 </div>
