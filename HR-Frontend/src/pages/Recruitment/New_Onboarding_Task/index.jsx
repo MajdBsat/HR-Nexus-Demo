2cee54406efcf0b6a5817247d2f2698cced52f69
Mohammad Zeineddine <mohammad.zeineddine50@gmail.com>
2025-03-24 10:57:59 +0200

    added add new onboarding task

--- HR-Frontend/src/pages/Recruitment/New_Onboarding_Task/index.jsx
+++ HR-Frontend/src/pages/Recruitment/New_Onboarding_Task/index.jsx
@@ -8,30 +8,42 @@ const Add_New_Onboarding_Task = () => {
         navigate("/hr/recruitment/onboarding/")
     }
 
-    const addJob = (e) =>{
+    const addTask = (e) =>{
         e.preventDefault()
         console.log('Add')
     }
     return(
-        <div className="back-div flex column center width100vw height100vh">
+        <div className="onboarding-task back-div flex column center width100vw height100vh">
             <form className="flex column center">
                 <h2 className="form_title">Add New Task</h2>
                 <div className="label_input flex column width50">
                     <label htmlFor="title">Task Title</label>
-                    <input id="title" type="text" placeholder="Deploy on server"/>
+                    <input id="title" name="title" type="text" placeholder="Deploy on server"/>
                 </div>
                 <div className="label_input flex column width50">
-                    <label htmlFor="description">Job Description</label>
-                    <textarea rows={10} id="description" type="text" placeholder="We are seeking a skilled and detail-oriented Programmer to join our development team."/>
-                </div>
-                <div className="label_input flex column width50">
-                    <label htmlFor="requirements">Job Requirements</label>
-                    <textarea rows={10} id="requirements" type="text" placeholder="HTML, CSS, JS, PHP, Laravel"/>
+                    <label htmlFor="assign_to">Assign To</label>
+                    <input id="title" name="assign_to" type="text" placeholder="Deploy on server"/>
                 </div>
 
+                <div className="label_input flex column width50">
+                    <label>Due Date</label>
+                    <div className="input-group flex row center">
+                            <input type="date" id="date" className="width50"/>
+                            <input type="time" id="time" className="width50"/>
+                    </div>
+                </div>
+
+                <div className="label_input flex column width50">
+                    <label htmlFor="description">Priority</label>
+                    <select id="spinner-select" name="description" onchange="updateSpinner()">
+                        <option value="high">High</option>
+                        <option value="medium">Medium</option>
+                        <option value="low">Low</option>
+                    </select>
+                </div>
                 <div className="buttons flex row center width50">
                     <button className="cancel-btn" onClick={(e)=>cancel(e)}>Cancel</button>
-                    <button className="add-btn" onClick={(e)=>addJob(e)}>Add</button>
+                    <button className="add-btn" onClick={(e)=>addTask(e)}>Add</button>
                 </div>
             </form>
         </div>
